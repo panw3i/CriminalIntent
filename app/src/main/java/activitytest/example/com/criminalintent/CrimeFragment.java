365798dec73489fe83aeacfb26306eec555dbe0a
panw3i MBP <panw3i@gmail.com>
2017-08-24 22:04:50 +0800

    控件监听事件

--- app/src/main/java/activitytest/example/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/activitytest/example/com/criminalintent/CrimeFragment.java
@@ -8,6 +8,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 /**
@@ -17,6 +20,8 @@ import android.widget.TextView;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private TextView mTitle;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,7 +36,8 @@ public class CrimeFragment extends Fragment {
 
         mTitle = (TextView) view.findViewById(R.id.crime_title);
 
-        //
+
+        // 文本监听事件
         mTitle.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -46,6 +52,20 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        // 先设置时间按纽为禁用状态 并设定实例对象创建时的日期
+        mDateButton = (Button) view.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        // CheckBox 的监听事件
+        mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
