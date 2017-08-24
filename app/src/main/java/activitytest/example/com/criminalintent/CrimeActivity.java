5b50e2bc8c0ad4913f175b8f16e51dbaea502052
panw3i MBP <panw3i@gmail.com>
2017-08-24 22:25:31 +0800

    使用抽象类创建 Fragment

--- app/src/main/java/activitytest/example/com/criminalintent/CrimeActivity.java
+++ app/src/main/java/activitytest/example/com/criminalintent/CrimeActivity.java
@@ -1,23 +1,12 @@
 package activitytest.example.com.criminalintent;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment ==null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
